084b824641a2b60c29b66895b14e9ee4c413758a
JackelineGois <jackelinedemoraisgois@gmail.com>
2024-02-28 14:33:42 +0000

    Using standard DTO to return Users

--- demo/src/main/java/com/project/demo/resources/UserResource.java
+++ demo/src/main/java/com/project/demo/resources/UserResource.java
@@ -1,8 +1,10 @@
 package com.project.demo.resources;
 
 import com.project.demo.domain.User;
+import com.project.demo.dto.UserDTO;
 import com.project.demo.service.UserService;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,8 +19,12 @@ public class UserResource {
   private UserService service;
 
   @GetMapping
-  public ResponseEntity<List<User>> findAll() {
+  public ResponseEntity<List<UserDTO>> findAll() {
     List<User> list = service.findAll();
-    return ResponseEntity.ok().body(list);
+    List<UserDTO> listDto = list
+      .stream()
+      .map(x -> new UserDTO(x))
+      .collect(Collectors.toList());
+    return ResponseEntity.ok().body(listDto);
   }
 }
