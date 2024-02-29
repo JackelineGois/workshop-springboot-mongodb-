28f5bead767ea0afea9ad8e888a90e74abd5e587
JackelineGois <jackelinedemoraisgois@gmail.com>
2024-02-29 13:33:26 +0000

    Referencing user posts

--- demo/src/main/java/com/project/demo/config/Instantiation.java
+++ demo/src/main/java/com/project/demo/config/Instantiation.java
@@ -51,6 +51,9 @@ public class Instantiation implements CommandLineRunner {
     );
 
     postRepository.saveAll(Arrays.asList(post1, post2));
+    maria.getPosts().addAll(Arrays.asList(post1, post2));
+
+    userRepository.save(maria);
   }
 
   private Instant formattedInstant(String string) {
