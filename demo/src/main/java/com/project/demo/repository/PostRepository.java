015cf29a82041ea026e894dda754555844fb3937
JackelineGois <jackelinedemoraisgois@gmail.com>
2024-03-01 13:32:31 +0000

    Simple query with @Query

--- demo/src/main/java/com/project/demo/repository/PostRepository.java
+++ demo/src/main/java/com/project/demo/repository/PostRepository.java
@@ -3,8 +3,12 @@ package com.project.demo.repository;
 import com.project.demo.domain.Post;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface PostRepository extends MongoRepository<Post, String> {
+  @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+  List<Post> searchTitle(String text);
+
   //Query methods
   List<Post> findByTitleContainingIgnoreCase(String text);
 }
