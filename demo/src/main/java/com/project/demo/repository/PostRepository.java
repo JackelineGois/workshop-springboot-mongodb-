d577181f833ecd850ddb19f4c816b7492aedcf5d
JackelineGois <jackelinedemoraisgois@gmail.com>
2024-03-01 11:46:14 +0000

    Simple query with Query methods

--- demo/src/main/java/com/project/demo/repository/PostRepository.java
+++ demo/src/main/java/com/project/demo/repository/PostRepository.java
@@ -1,6 +1,10 @@
 package com.project.demo.repository;
 
 import com.project.demo.domain.Post;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface PostRepository extends MongoRepository<Post, String> {}
+public interface PostRepository extends MongoRepository<Post, String> {
+  //Query methods
+  List<Post> findByTitleContainingIgnoreCase(String text);
+}
---
+++ demo/src/main/java/com/project/demo/service/PostService.java
@@ -0,0 +1,27 @@
+package com.project.demo.service;
+
+import com.project.demo.domain.Post;
+import com.project.demo.repository.PostRepository;
+import com.project.demo.service.exception.ObjectNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepository repo;
+
+  public Post findById(String id) {
+    Optional<Post> user = repo.findById(id);
+    return user.orElseThrow(() ->
+      new ObjectNotFoundException("Id não encontrado")
+    );
+  }
+
+  public List<Post> findByTitle(String txt) {
+    return repo.findByTitleContainingIgnoreCase(txt);
+  }
+}
